2657aba52a2140606220adaf3a2abdab20b58410
Malinda <greshanamarakoon@gmail.com>
2025-09-27 20:54:50 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import "./css/App.css";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <NavBar />
+      </div>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favorites.css";
+
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No favorites movies yet</h2>
+      <p>Start Adding Movies</p>
+    </div>
+  );
+}
+
+export default Favorites;
